bf72c6ff7d54486089d259b71c8a284e11ee14b9
Asamoah Gregory kwaku <99300623+gkasamoah@users.noreply.github.com>
2023-04-07 00:33:27 +0000

    Add files via upload
    This the main project code

---
+++ DrugVerificationSystem/backend/database/dbcon.php
@@ -0,0 +1,22 @@
+<?php 
+$username='gkasamoah';
+$host= 'localhost';
+$database='drugverification';
+$password='';
+$port=4000;
+// connecting to the database using the mysqli api
+$con=mysqli_connect($host,$username,'',$database,$port);
+
+if($con){
+    echo 'Connected successfully';
+ 
+}else{
+    
+    echo "Failed to connect";
+}
+
+
+
+
+
+?>
---
+++ DrugVerificationSystem/public/Homepage/homepage.php
@@ -0,0 +1,53 @@
+<?php
+require_once('C:\Users\Asamoah Gregory\Desktop\xamp\htdocs\DrugVerificationSystem\public\header.html');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   
+
+</head>
+<body>
+    <!--The hero Section-->
+    <div class=" container-fluid" id="SearchDrug">
+        <img src="https://img.freepik.com/free-photo/flat-lay-medical-composition-with-clipboard-template_23-2148124585.jpg?size=626&ext=jpg&ga=GA1.1.1732100777.1679974209&semt=ais" alt="Hero-image" width="100%" height="300px">
+
+
+        <div style="background-color: #DBE4EB; margin: 25px;" class=" d-flex justify-content-center"> <div>
+            <p class=" fw-bolder fs-1">
+            Enter the name of the drug Here</p>
+            <form action="">
+                <input type="search" name="" id="" placeholder="Drug names" class=" form-control"> <br> <button type="submit" class=" btn-success btn-md">Check</button>
+            </form>
+        </div>
+    </div>  
+
+
+
+   <?php include_once('C:\Users\Asamoah Gregory\Desktop\xamp\htdocs\DrugVerificationSystem\public\footer.html') ?>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
